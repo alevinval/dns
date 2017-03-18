4a5cff6b61208c45854ce4d757a301b467a9b385
Àlex Vinyals Valdepeñas <alevinval@gmail.com>
2017-03-18 15:42:05 +0100

    (wip) cleanup packer

--- packer.go
+++ packer.go
@@ -5,36 +5,35 @@ import "bytes"
 func packHeader(h *Header) []byte {
 	var flags uint16
 	if h.QR {
-		flags += 1 << maskQROffset
+		flags |= 1 << maskQROffset
 	}
-	flags += uint16(h.OpCode) << maskOpCodeOffset
+	flags |= uint16(h.OpCode) << maskOpCodeOffset
 	if h.AA {
-		flags += 1 << maskAAOffset
+		flags |= 1 << maskAAOffset
 	}
 	if h.TC {
-		flags += 1 << maskTCOffset
+		flags |= 1 << maskTCOffset
 	}
 	if h.RD {
-		flags += 1 << maskRDOffset
+		flags |= 1 << maskRDOffset
 	}
 	if h.RA {
-		flags += 1 << maskRAOffset
+		flags |= 1 << maskRAOffset
 	}
-	flags += uint16(h.Z) << maskZOffset
-	flags += uint16(h.RCode)
+	flags |= uint16(h.Z) << maskZOffset
+	flags |= uint16(h.RCode)
 
-	header := bytes.NewBuffer(make([]byte, 0, headerLen))
-	header.WriteByte(byte(h.ID >> 8))
-	header.WriteByte(byte(h.ID))
-	header.WriteByte(byte(flags >> 8))
-	header.WriteByte(byte(flags))
-	header.WriteByte(byte(h.QDCount >> 8))
-	header.WriteByte(byte(h.QDCount))
-	header.WriteByte(byte(h.ANCount >> 8))
-	header.WriteByte(byte(h.ANCount))
-	header.WriteByte(byte(h.NSCount >> 8))
-	header.WriteByte(byte(h.NSCount))
-	header.WriteByte(byte(h.ARCount >> 8))
-	header.WriteByte(byte(h.ARCount))
-	return header.Bytes()
+	buffer := bytes.NewBuffer(make([]byte, 0, headerLen))
+	writeUint16(buffer, h.ID)
+	writeUint16(buffer, flags)
+	writeUint16(buffer, h.QDCount)
+	writeUint16(buffer, h.ANCount)
+	writeUint16(buffer, h.NSCount)
+	writeUint16(buffer, h.ARCount)
+	return buffer.Bytes()
+}
+
+func writeUint16(b *bytes.Buffer, v uint16) {
+	b.WriteByte(byte(v >> 8))
+	b.WriteByte(byte(v))
 }
